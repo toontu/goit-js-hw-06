9852cfb9f2f4caa4e6aacd3b00b30004a9567ed9
Maryna Ushakova <ginger4795@gmail.com>
2022-07-05 20:34:33 +0300

    task 02

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const categoryList = document.querySelector('#categories');
+const categoryListEl = document.querySelector('#categories');
 
-const categoryListItems = [...categoryList.children];
+const categoryListItemsEl = [...categoryListEl.children];
 
-console.log(`Number of categories: ${categoryListItems.length}`);
+console.log(`Number of categories: ${categoryListItemsEl.length}`);
 
-categoryListItems.forEach(el => {
-  const nestedLiItems = el.querySelectorAll('li');
-  const nestedH2TextContent = el.querySelector('h2').textContent;
+categoryListItemsEl.forEach(el => {
+  const nestedLiItemsEl = el.querySelectorAll('li');
+  const nestedH2TextContentEl = el.querySelector('h2').textContent;
 
-  console.log(`Category: ${nestedH2TextContent}`);
-  console.log(`Elements: ${nestedLiItems.length}`);
+  console.log(`Category: ${nestedH2TextContentEl}`);
+  console.log(`Elements: ${nestedLiItemsEl.length}`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsArrayEl = document.querySelector(ingredients);
+
+const ingredientsItemsEl = ingredients.map(el => {
+  const ingredientsItemsEl = document.createElement('li');
+
+  ingredientsItemsEl.classList.add('item');
+  ingredientsItemsEl.textContent = el;
+
+  return ingredientsItemsEl;
+});
+
+const ingredientsListEl = document.createElement('ul');
+ingredientsListEl.id = 'ingredients';
+
+ingredientsListEl.append(...ingredientsItemsEl);
+
+console.log(ingredientsListEl);
