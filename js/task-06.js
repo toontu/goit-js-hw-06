f61fa61733c9eb533f9b3545720403c6083cd459
Maryna Ushakova <ginger4795@gmail.com>
2022-07-06 19:14:10 +0300

    task 06 added

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,19 @@
 const inputEl = document.querySelector('#validation-input');
 
-inputEl.addEventListener('blur');
+inputEl.addEventListener('blur', () => {
+  const inputNeedeLength = Number(inputEl.dataset.length);
+
+  if (inputEl.value.length === inputNeedeLength) {
+    checkAndRemove('invalid');
+    inputEl.classList.add('valid');
+  } else {
+    checkAndRemove('valid');
+    inputEl.classList.add('invalid');
+  }
+});
+
+const checkAndRemove = className => {
+  if (inputEl.classList.contains(className)) {
+    inputEl.classList.remove(className);
+  }
+};
