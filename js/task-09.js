e67564ddb1ea6a4724d291f90f1242a7286e3e90
Maryna Ushakova <ginger4795@gmail.com>
2022-07-06 20:28:45 +0300

    task 09 added

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const spanColorEl = document.querySelector('span.color');
+const changeColorBtn = document.querySelector('button.change-color');
+
+changeColorBtn.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  spanColorEl.textContent = bodyEl.style.backgroundColor;
+});
