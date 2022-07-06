57572a0e30df74ffc5a3d382556fd07589b8bfa3
Maryna Ushakova <ginger4795@gmail.com>
2022-07-06 21:33:46 +0300

    task 08 added

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const inputFormEl = document.querySelector('form.login-form');
 
+inputFormEl.addEventListener('submit', onInputFormSubmit);
+
+function onInputFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Будьласка, заповніть всі поля');
+  }
+
+  console.log({ email: email.value, password: password.value });
+  event.currentTarget.reset();
+}
