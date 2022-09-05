45f2d18b60d4fcade965b812140d24f7364b5550
Maryna Ushakova <ginger4795@gmail.com>
2022-09-05 23:08:10 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,34 @@
+const categoryEl = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${categoryEl.length}`);
+// console.log(categoryEl);
+
+categoryEl.forEach(el => {
+  //   console.log(el);
+  const categoryTitle = el.querySelector('h2').textContent;
+  console.log(`Category: ${categoryTitle}`);
+
+  const categoryItemsNumber = el.querySelectorAll('li').length;
+  console.log(`Elements: ${categoryItemsNumber}`);
+});
+
+// ================
+// Вариант 2:
+
+// const categoryListEl = document.querySelector('#categories');
+// // console.log(categoryListEl);
+
+// const categoryListItemsEl = [...categoryListEl.children];
+// // console.log(categoryListItemsEl);
+
+// console.log(`Number of categories: ${categoryListItemsEl.length}`);
+
+// categoryListItemsEl.forEach(el => {
+//   const nestedLiItemsEl = el.querySelectorAll('li');
+//   //   console.log(nestedLiItemsEl);
+
+//   const nestedH2TextContentEl = el.querySelector('h2').textContent;
+//   //   console.log(nestedH2TextContentEl);
+
+//   console.log(`Category: ${nestedH2TextContentEl}`);
+//   console.log(`Elements: ${nestedLiItemsEl.length}`);
+// });
