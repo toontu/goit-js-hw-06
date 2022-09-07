fe08af56d9abab9f8f121f036834ba47607e02ab
Maryna Ushakova <ginger4795@gmail.com>
2022-09-07 21:54:31 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -11,3 +11,41 @@ function getRandomHexColor() {
 // Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
 
 // Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+
+const inputEl = document.querySelector('input');
+const createBoxesBtn = document.querySelector('[data-create]');
+const destroyBoxesBtn = document.querySelector('[data-destroy]');
+const boxesCollectionEl = document.querySelector('#boxes');
+
+const createBoxes = amount => {
+  amount = Number(inputEl.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    const boxDiv = document.createElement('div');
+    const boxColor = getRandomHexColor();
+    const boxSize = 30;
+
+    // boxDiv.style.width = `${boxSize + i * 10}px`;
+    // boxDiv.style.height = `${boxSize + i * 10}px`;
+    // boxDiv.style.backgroundColor = boxColor;
+    // console.dir(boxDiv);
+
+    // boxDiv.style.cssText = `width: ${boxSize + i * 10}px; height: ${
+    //   boxSize + i * 10
+    // }px; background-color: ${boxColor}; margin: 7px`;
+
+    boxDiv.style = `width: ${boxSize + i * 10}px; height: ${
+      boxSize + i * 10
+    }px; background-color: ${boxColor}; margin: 7px`;
+
+    boxesCollectionEl.append(boxDiv);
+    inputEl.value = '';
+  }
+};
+const destroyBoxes = () => {
+  boxesCollectionEl.innerHTML = '';
+  inputEl.value = '';
+};
+
+createBoxesBtn.addEventListener('click', createBoxes);
+destroyBoxesBtn.addEventListener('click', destroyBoxes);
