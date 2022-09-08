931fbc0c525d9c0ea0d7c46c74b9701d8bfc91e8
Maryna Ushakova <ginger4795@gmail.com>
2022-09-08 19:25:46 +0300

    task 10 - mentor's solution added

--- js/task-10.js
+++ js/task-10.js
@@ -49,3 +49,44 @@ const destroyBoxes = () => {
 
 createBoxesBtn.addEventListener('click', createBoxes);
 destroyBoxesBtn.addEventListener('click', destroyBoxes);
+
+//
+// ====== решение ментора:
+
+// const inputRef = document.querySelector('[type="number"]');
+// const createdRef = document.querySelector('[data-create]');
+// const destroyRef = document.querySelector('[data-destroy]');
+// const boxesRef = document.querySelector('#boxes');
+
+// function onCreateMarkup() {
+//   const value = inputRef.value;
+//   if (!value) return;
+//   const markup = createMarkup(value);
+//   updateMarkup(markup);
+// }
+
+// function createMarkup(payload) {
+//   let size = 30;
+//   const markup = [];
+//   for (let i = 0; i < Number(payload); i++) {
+//     markup.push(
+//       `<div class="item" style="width:${size}px; height:${size}px; background: ${getRandomHexColor()}"></div>`
+//     );
+//     size += 10;
+//   }
+//   return markup.join('');
+// }
+
+// function updateMarkup(markup = '') {
+//   boxesRef.innerHTML = markup;
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// createdRef.addEventListener('click', onCreateMarkup);
+// destroyRef.addEventListener('click', () => {
+//   inputRef.value = '';
+//   updateMarkup();
+// });
